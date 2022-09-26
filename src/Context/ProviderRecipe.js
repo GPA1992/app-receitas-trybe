b9a6eeca8483dc53ad49580777a363faef1229a9
Gabriel <gabrielproencaaraujo@gmail.com>
2022-09-26 17:00:50 -0300

    Package corrigido e context atualizado

--- src/Context/ProviderRecipe.js
+++ src/Context/ProviderRecipe.js
@@ -4,17 +4,12 @@ import ContextRecipe from './ContextRecipe';
 function Provider({ children }) {
   const [state, setState] = useState(INITIAL_STATE);
 
-  const context = useMemo(() => ({
-    state,
-    setState,
-  }), [state]);
-
-  /*   const context = {
+  const context = {
     state,
     setState,
     test,
     setTest,
-  }; */
+  };
 
   return (
     <ContextRecipe.Provider value={ context }>
