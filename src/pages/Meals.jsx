e0a881597b2c9f44ad2f3afce34147b9cc82a12e
PauloMisaki <ph.sorata@gmail.com>
2022-09-27 17:03:33 -0300

    remove log

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -4,13 +4,12 @@ import Recipes from '../Components/Recipes';
 import fetchMealsCategories from '../services/fetchMealsCategories';
 
 export default function Meals() {
-  const { title, setTitle, mealsCategories, setMealsCategories } = useContext(context);
+  const { title, setTitle, setMealsCategories } = useContext(context);
 
   useEffect(() => {
     async function getMealsCategories() {
       const data = await fetchMealsCategories();
       setMealsCategories(data.meals);
-      console.log(mealsCategories);
     }
     getMealsCategories();
     setTitle('Meals');
