7dfdbb5ed010975cff38919c92264478d9f971cc
Edvaldo Alves do Nascimento <edvaldodentt@gmail.com>
2022-10-03 07:50:13 -0300

    Ajuste de linter

--- src/tests/ShareAndFavoriteButtons.test.js
+++ src/tests/ShareAndFavoriteButtons.test.js
@@ -3,9 +3,10 @@ import userEvent from '@testing-library/user-event';
 
 import renderWithRouter from './utils/renderWithRouter';
 import App from '../App';
-import { DESCRIPTION_MEAL_PATHNAME, DESCRIPTION_DRINK_PATHNAME, RECIPE_PHOTO_TEST_ID } from './utils/constantsTest';
+import { DESCRIPTION_MEAL_PATHNAME, DESCRIPTION_DRINK_PATHNAME, RECIPE_PHOTO_TEST_ID,
+  SHARE_BTN_TEST_ID, FAVORITE_BTN_TEST_ID, FAVORITE_KEY_LOCAL_STORAGE } from './utils/constantsTest';
 import { MOCK_FAVORITE_RECIPE_MEAL_BEFORE, MOCK_FAVORITE_RECIPE_MEAL_AFTER,
-  MOCK_FAVORITE_RECIPE_DRINK_BEFORE, MOCK_FAVORITE_RECIPE_DRINK_AFTER } from './utils/utilsMocks';
+  MOCK_FAVORITE_RECIPE_DRINK_BEFORE } from './utils/utilsMocks';
 import fetch from '../../cypress/mocks/fetch';
 import { pushInLocalStorage, getFromLocalStorage } from '../service/localStorage';
 
@@ -18,13 +19,13 @@ describe('Testa o componente ShareAndFavoriteButtons.', () => {
     const recipePhoto = await screen.findByTestId(RECIPE_PHOTO_TEST_ID);
     expect(recipePhoto).toBeInTheDocument();
 
-    const whiteHeartButton = await screen.findByTestId('favorite-btn');
+    const whiteHeartButton = await screen.findByTestId(FAVORITE_BTN_TEST_ID);
     expect(whiteHeartButton).toBeInTheDocument();
     expect(whiteHeartButton).toHaveAttribute('src', 'whiteHeartIcon.svg');
 
     userEvent.click(whiteHeartButton);
 
-    const blackHeartButton = await screen.findByTestId('favorite-btn');
+    const blackHeartButton = await screen.findByTestId(FAVORITE_BTN_TEST_ID);
     expect(blackHeartButton).toBeInTheDocument();
     expect(blackHeartButton).toHaveAttribute('src', 'blackHeartIcon.svg');
   });
@@ -37,7 +38,7 @@ describe('Testa o componente ShareAndFavoriteButtons.', () => {
 
     renderWithRouter(<App />, DESCRIPTION_DRINK_PATHNAME);
 
-    const shareButton = await screen.findByTestId('share-btn');
+    const shareButton = await screen.findByTestId(SHARE_BTN_TEST_ID);
     expect(shareButton).toBeInTheDocument();
 
     userEvent.click(shareButton);
@@ -49,17 +50,17 @@ describe('Testa o componente ShareAndFavoriteButtons.', () => {
   test('Testa se, ao clicar no botão de favoritos, a receita é adicionada à lista de favoritos. ', () => {
     jest.spyOn(global, 'fetch').mockImplementation(fetch);
 
-    pushInLocalStorage('favoriteRecipes', MOCK_FAVORITE_RECIPE_MEAL_BEFORE);
+    pushInLocalStorage(FAVORITE_KEY_LOCAL_STORAGE, MOCK_FAVORITE_RECIPE_MEAL_BEFORE);
 
     renderWithRouter(<App />, DESCRIPTION_MEAL_PATHNAME);
-    const favoriteListBefore = getFromLocalStorage('favoriteRecipes');
+    const favoriteListBefore = getFromLocalStorage(FAVORITE_KEY_LOCAL_STORAGE);
 
     expect(favoriteListBefore).not.toBe(null);
 
-    const blackHeartButton = screen.getByTestId('favorite-btn');
+    const blackHeartButton = screen.getByTestId(FAVORITE_BTN_TEST_ID);
     userEvent.click(blackHeartButton);
 
-    const favoriteListAfter = getFromLocalStorage('favoriteRecipes');
+    const favoriteListAfter = getFromLocalStorage(FAVORITE_KEY_LOCAL_STORAGE);
 
     expect(favoriteListAfter).toEqual(MOCK_FAVORITE_RECIPE_MEAL_AFTER);
   });
@@ -67,23 +68,23 @@ describe('Testa o componente ShareAndFavoriteButtons.', () => {
   test('Testa se, ao clicar no botão de favoritos, a receita é adicionada à lista de favoritos. ', async () => {
     jest.spyOn(global, 'fetch').mockImplementation(fetch);
 
-    pushInLocalStorage('favoriteRecipes', MOCK_FAVORITE_RECIPE_DRINK_BEFORE);
+    pushInLocalStorage(FAVORITE_KEY_LOCAL_STORAGE, MOCK_FAVORITE_RECIPE_DRINK_BEFORE);
 
     renderWithRouter(<App />, DESCRIPTION_DRINK_PATHNAME);
-    const favoriteListBefore = getFromLocalStorage('favoriteRecipes');
+    const favoriteListBefore = getFromLocalStorage(FAVORITE_KEY_LOCAL_STORAGE);
     expect(favoriteListBefore).toEqual([]);
 
     const recipePhoto = await screen.findByTestId(RECIPE_PHOTO_TEST_ID);
     expect(recipePhoto).toBeInTheDocument();
 
-    const whiteHeartButton = screen.getByTestId('favorite-btn');
+    const whiteHeartButton = screen.getByTestId(FAVORITE_BTN_TEST_ID);
     userEvent.click(whiteHeartButton);
 
-    const favoriteListAfter = getFromLocalStorage('favoriteRecipes');
+    const favoriteListAfter = getFromLocalStorage(FAVORITE_KEY_LOCAL_STORAGE);
 
     expect(favoriteListAfter[0].id).toBe('15997');
 
-    const blackHeartButton = await screen.findByTestId('favorite-btn');
+    const blackHeartButton = await screen.findByTestId(FAVORITE_BTN_TEST_ID);
     expect(blackHeartButton).toBeInTheDocument();
     expect(blackHeartButton).toHaveAttribute('src', 'blackHeartIcon.svg');
   });
