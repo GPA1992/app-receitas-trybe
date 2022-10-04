bca3bd7a21fcb26e3c180a6146d8ee36991aa468
Gabriel <gabrielproencaaraujo@gmail.com>
2022-10-04 17:22:11 -0300

    Testes Footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -4,25 +4,19 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
 
-// Não foi necessário colocar o valor de placeholder e dos userType,
-// pois cada valor foi só usado uma única vez, assim não disparou
-// Nenhum aviso no lint
+const drinksButtonID = 'drinks-btn';
+const mealsButtonID = 'meals-btn';
+
 describe('O componente footer deve ', () => {
   it('ser encontrado na rota /meals', () => {
-    renderWithRouter(<App />);
-    const emailInput = screen.getByPlaceholderText('exemplo@exemplo.com');
-    const passwordInput = screen.getByPlaceholderText(/senha/i);
-    const button = screen.getByTestId('login-submit-btn');
-    userEvent.type(emailInput, 'email@email.com');
-    userEvent.type(passwordInput, '1234567');
-    userEvent.click(button);
-
-    const footer = screen.getByTestId('footer');
-    expect(footer).toBeInTheDocument();
-
-    const drinksBtn = screen.getByTestId('drinks-btn');
-    const mealsBtn = screen.getByTestId('meals-btn');
-    expect(drinksBtn).toBeInTheDocument();
-    expect(mealsBtn).toBeInTheDocument();
+    const { history } = renderWithRouter(<App />, '/meals');
+    const drinksButton = screen.getByTestId(drinksButtonID);
+    expect(drinksButton).toBeInTheDocument();
+    userEvent.click(drinksButton);
+    expect(history.location.pathname).toBe('/drinks');
+    const mealsButton = screen.getByTestId(mealsButtonID);
+    expect(mealsButton).toBeInTheDocument();
+    userEvent.click(mealsButton);
+    expect(history.location.pathname).toBe('/meals');
   });
 });
